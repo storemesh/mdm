840de94ca6a1cd9b6fa50f8e5b7affa5f03987df
patcharapon1995 <patcharapon1995@gmail.com>
2023-01-29 18:46:54 +0700

    [delete] unused files

---
+++ README.md
@@ -0,0 +1,11 @@
+## MDM Demo
+
+# Install
+- python3.8
+```
+pip install -r requirements.txt
+```
+
+# Run
+Run All Cell in
+- semantic_similarity.ipynb
--- utils.py
+++ utils.py
@@ -9,27 +9,6 @@ import hdbscan
 import time
 import umap
 import umap.plot
-# from line_profiler import LineProfiler
-
-# profile = LineProfiler()
-
-def prepare_dataset():
-    df = pd.read_parquet('dataset/semantic_similarity/hscode.parquet')
-    df_hscode2022 = df[df['HSYear'] == 2022]
-    df_hscode4_th = df_hscode2022[['thdescriptions4','HSCode2']]
-    df_hscode4_th = df_hscode4_th.rename(columns={'thdescriptions4': 'hscode4_text'})
-
-    df_hscode4_en = df_hscode2022[['endescriptions4','HSCode2']]
-    df_hscode4_en = df_hscode4_en.rename(columns={'endescriptions4': 'hscode4_text'})
-
-    df_hscode4 = pd.concat([
-        df_hscode4_th,
-        df_hscode4_en
-    ])
-    df_hscode4 = df_hscode4.drop_duplicates()
-    df_hscode4 = df_hscode4[df_hscode4['hscode4_text'].notnull()]
-    df_hscode4 = df_hscode4.reset_index().drop(columns='index')
-    return df_hscode4
 
 def prepare_dataset_en():
     df = pd.read_csv('dataset/semantic_similarity/hscode4_en_translate.csv', dtype={'HSCode2': 'string'})
@@ -38,47 +17,6 @@ def prepare_dataset_en():
     df['HSCode2'] = df['HSCode2'].str.zfill(2)
     return df
     
-
-def find_accuracy(series):    
-    n = 2
-    count_series = series.value_counts()
-    sum_all = count_series.sum()
-    count_series_no_noise = count_series[count_series.index != -1]
-    sum_top_n = count_series_no_noise.iloc[:n].sum()
-    accuracy = sum_top_n / sum_all
-    
-    return accuracy
-
-
-def calculate_all_metric(df_hscode4, X):
-    acc_each_hscode = df_hscode4.groupby('HSCode2')['cluster_group_id'].apply(find_accuracy)
-    acc_mean = acc_each_hscode.mean()
-    
-    df_result = df_hscode4.copy()
-    count_cluster = df_result['cluster_group_id'].value_counts() 
-    
-    n_each_cluster = count_cluster[count_cluster.index != -1]
-    cluster_size = n_each_cluster.shape[0]
-    n_top_20_cluster = n_each_cluster.iloc[:20].values
-    
-    n_noise = count_cluster[count_cluster.index == -1].sum()
-    
-    if cluster_size == 0:
-        silhouette = 0.0
-    else:
-        silhouette = metrics.silhouette_score(X, df_hscode4['cluster_group_id'].values, metric='euclidean')
-    
-    dict_result = {
-        'acc_mean': acc_mean,
-        'silhouette': silhouette,
-        'cluster_size': cluster_size,
-        'n_noise': n_noise,
-        'n_top_20_cluster': n_top_20_cluster,
-    }
-    
-    return dict_result, df_result
-
-# @profile
 def find_spearman_pearson(X, labels):
     cos_sim = util.cos_sim(X, X)
     dot_products = util.dot_score(X,X)
